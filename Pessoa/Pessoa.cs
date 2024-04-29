d9226e82e9dba831f25c7719bd481e7e81eacdc1
Luan-art <luanlf2003@gmail.com>
2024-04-29 13:23:48 -0300

    Add project files.

---
+++ Pessoa/Pessoa.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Pessoa
+{
+    public string nome;
+    public string sobrenome;
+    public int idade;
+    public string telefone;
+    public string celular;
+    public string email;
+
+
+
+    public Pessoa()
+    {
+
+
+    }
+
+    public string DefinirNome()
+    {
+        Console.WriteLine("Informe nome desejado");
+        return Console.ReadLine();
+    }
+
+    public string DefinirSobrenome()
+    {
+        Console.WriteLine("Informe sobrenome desejado");
+        return Console.ReadLine();
+    }
+
+    public int DefinirIdade()
+    {
+        Console.WriteLine("Informe a idade desejada ");
+        return int.Parse(Console.ReadLine());
+    }
+
+    public void DefinirTelefone(string telefone)
+    {
+        this.telefone = telefone;
+    }
+
+
+    public void DefinirCelular(string celular)
+    {
+        this.celular = celular;
+    }
+
+
+    public void DefinirEmail(string email)
+    {
+        this.email = email;
+
+    }
+
+    public void MostrarDados()
+    {
+        Console.WriteLine($"Nome: {nome}", this.nome);
+        Console.WriteLine($"Sobrenom: {sobrenome}", this.sobrenome);
+        Console.WriteLine($"Idade: {idade}", this.idade);
+        Console.WriteLine($"Telefone: {telefone}", this.telefone);
+        Console.WriteLine($"Celular: {celular}", this.celular);
+        Console.WriteLine($"Email: {email}", this.email);
+
+    }
+
+
+
+
+
+
+}
+
+
+
+
---
+++ Pessoa/Program.cs
@@ -0,0 +1,41 @@
+﻿
+Pessoa pessoa = PreencherPessoa();
+
+MostrarPessoa(pessoa);
+
+Pessoa pessoa1 = PreencherPessoa();
+
+MostrarPessoa(pessoa1);
+
+Pessoa pessoa2 = PreencherPessoa();
+
+
+MostrarPessoa(pessoa2);
+
+Pessoa PreencherPessoa()
+{
+    Pessoa pessoa = new Pessoa();
+
+    pessoa.nome = pessoa.DefinirNome();
+    pessoa.idade = pessoa.DefinirIdade();
+    pessoa.sobrenome = pessoa.DefinirSobrenome();
+
+    Console.WriteLine("Informw au telefone");
+    pessoa.DefinirTelefone(Console.ReadLine());
+
+    Console.WriteLine("Informw au celular");
+    pessoa.DefinirCelular(Console.ReadLine());
+
+    Console.WriteLine("Informw au rmail");
+    pessoa.DefinirEmail(Console.ReadLine());
+
+    return pessoa;
+}
+
+void MostrarPessoa(Pessoa pessoa)
+{
+    Console.ReadKey();
+    pessoa.MostrarDados();
+    Console.ReadKey();
+
+}
